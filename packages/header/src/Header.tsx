b92ef006282334473330a0d4b05be450184004fb
Robert Watt <rob.watt@sitecore.com>
2022-11-02 15:07:42 -0500

    Make changes to test publishing

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! Test Change
+            Lerna is the original monorepo tool! Publish Test
         </header>
     );
 }
